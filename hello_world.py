b2f4acc1feb47dc8ac76f2ad4fe611f757399b4e
Natalia Villegas-Franco <natalia.villegasfranco@ucdenver.edu>
2016-09-06 15:43:09 -0600

    fixed the hello world file so that it actually runs.

--- hello_world.py
+++ hello_world.py
@@ -3,21 +3,19 @@ str="Hello M5660,"
 #In python you can concatentate strings using the addition(+) operator
 print(str+" my name is Varis Carey")
 #Add your hello statment below this line of code
-print '\t my name is Daniel Bielich and I'm a pusher and I'm standing still.'
+print '\t my name is Daniel Bielich and Im a pusher and Im standing still.'
 print '\t my name is Michael Pilosov (first!)'
 print '\t my name is Jordan Hall'
-print '\t My name is Megan Sorenson and I'm a pusher!'
+print '\t My name is Megan Sorenson and Im a pusher!'
 print '\t my name is Jing Hu'
 print '\t my name is Mitchell Matheny and Im a pusher and im standing still '
-print "\t my name is Yuanlong Wang and I'm a pusher."
-print '\t ny name is Elise Reed and I am a pusher.'
-=======
-print "\t my name is Yuanlong Wang and I'm a pusher."'
+print '\t my name is Yuanlong Wang and Im a pusher.'
+print '\t my name is Elise Reed and I am a pusher.'
+print '\t my name is Yuanlong Wang and Im a pusher.'
 print '\t my name is Elise Reed'
->>>>>>> Stashed changes
 print '\t my name is Chau Nguyen'
-print "\t My name is Doug Cheney and I'm a pusher."
+print "\t My name is Doug Cheney and Im a pusher."
 print '\t my name is Alexa Desautels'
-print '\t my name is Natalia Villegas-Franco I'm a pusher and I'm standing still.'
+print '\t my name is Natalia Villegas-Franco I am the person who fixed the syntax in this file so it actually runs now'
 print '\t My name is Shanon Yeung.'
-print(str+" my name is Evan Shapiro")
+print '\t my name is Evan Shapiro'
