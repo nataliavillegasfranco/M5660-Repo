3bddaa95456ce19e8b84eb8b0b0cea2491110c1b
dbielich <daniel.bielich@ucdenver.edu>
2016-09-06 10:09:38 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -3,13 +3,13 @@ str="Hello M5660,"
 #In python you can concatentate strings using the addition(+) operator
 print(str+" my name is Varis Carey")
 #Add your hello statment below this line of code
+print '\t my name is Daniel Bielich and I'm a pusher and I'm standing still.'
 print '\t my name is Michael Pilosov (first!)'
 print '\t my name is Jordan Hall'
 print '\t My name is Megan Sorenson'
 print '\t my name is Jing Hu'
 print "\t my name is Yuanlong Wang and I'm a pusher."
 print '\t my name is Mitchell Matheny'  
-print '\t my name is Daniel Bielich and I'm a pusher'
 print '\t my name is Elise Reed'
 print '\t my name is Chau Nguyen'
 print '\t my name is Natalia Villegas-Franco.'
