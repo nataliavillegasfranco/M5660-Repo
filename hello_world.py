b6c111fddcd1f060cbce55a5b38f8c24c5269f92
dbielich <daniel.bielich@ucdenver.edu>
2016-09-06 10:04:26 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -9,7 +9,7 @@ print '\t My name is Megan Sorenson'
 print '\t my name is Jing Hu'
 print "\t my name is Yuanlong Wang and I'm a pusher."
 print '\t my name is Mitchell Matheny'  
-print '\t my name is Daniel Bielich'
+print '\t my name is Daniel Bielich and I'm a pusher'
 print '\t my name is Elise Reed'
 print '\t my name is Chau Nguyen'
 print '\t my name is Natalia Villegas-Franco.'
